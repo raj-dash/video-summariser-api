f926331c16c26ad18edc9e602a3d56d78c6b80bd
Raj Aryan Dash <rajaryanrad@gmail.com>
2026-05-31 00:20:15 +0530

    completed the summarisation service

--- app/services/summarisation_service.py
+++ app/services/summarisation_service.py
@@ -14,4 +14,25 @@ class SummarisationService:
         llm: LlmService,
         transcribing_service: TranscribingService,
     ):
-        download_service.download_video(url=url)
+        path = download_service.download_video(url=url)
+        transcript = transcribing_service.transcribe(file=path)
+        summary = llm.llm_response(query=transcript)
+
+        return summary
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    llm = LlmService()
+    t = TranscribingService()
+    d = DownloadService()
+    s = SummarisationService()
+    summary = s.summary(
+        url="https://www.youtube.com/watch?v=g291trLaqTA",
+        download_service=d,
+        llm=llm,
+        transcribing_service=t,
+    )
+    print(summary)
